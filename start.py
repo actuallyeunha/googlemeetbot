b3dd0c2d139a283342edd63dbfc6927e3c7bb94b
Ana Eunha <66442365+actuallyeunha@users.noreply.github.com>
2020-10-08 13:39:42 -0300

    Update start.py
    - Fixing timings
    - Making it prettier

--- start.py
+++ start.py
@@ -1,7 +1,6 @@
 from getpass import getpass
 from colorama import init, deinit
 from colorama import Fore, Style
-import time
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
@@ -9,10 +8,16 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+import time
+from time import perf_counter
 import pause
 import pynput
 import os
 from pynput.keyboard import Key, Controller
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def title():
@@ -38,33 +43,40 @@ def login():
     username.send_keys(email)
     nextButton = browser.find_element_by_id('identifierNext')
     nextButton.click()
-    time.sleep(5)
+    time.sleep(1)
     password = browser.find_element_by_xpath("//input[@class='whsOnd zHQkBf']")
     password.send_keys(senha)
     signInButton = browser.find_element_by_id('passwordNext')
     signInButton.click()
 
 
+clear()
 init()
 title()
 t = time.localtime()
-current_time = time.strftime("%H:%M", t)
+current_time = time.strftime("%a | %d/%b/%y | %H:%M", t)
+prefix = Fore.MAGENTA + Style.BRIGHT + "[+] " + Fore.BLUE + Style.BRIGHT
 
 # Get Credentials
-print(Fore.BLUE + Style.BRIGHT + "Por favor, insira seu endereco de email. (ex: ana@eunha.com)")
+print(prefix + "Por favor, insira seu endereco de email. (ex: ana@eunha.com)")
 email = input("> ")
 
-print("Por favor, insira sua senha.")
+print()
+
+print(prefix + "Por favor, insira sua senha.")
 senha = getpass("> ")
+
+clear()
+title()
 
 # Main menu
 print()
-print("Seja bem vinde " + Fore.MAGENTA + Style.NORMAL + email + Fore.BLUE + Style.BRIGHT + "!")
-print("Sao: " + Fore.MAGENTA + Style.NORMAL + current_time)
+print(Fore.CYAN + Style.BRIGHT + current_time)
+print(prefix + "Seja bem vinde " + Fore.MAGENTA + Style.BRIGHT + email + Fore.BLUE + Style.BRIGHT + "!")
 print()
 
 print()
-print()
+print(prefix + "Pre-configuring" + Fore.YELLOW + Style.BRIGHT + " firefox" + Fore.BLUE + Style.BRIGHT + "...")
 
 # Set Options and log in
 options = Options()
@@ -74,4 +86,12 @@ options.set_preference("permissions.default.camera", 2)
 browser = webdriver.Firefox(options=options)
 browser.set_window_size(1280, 720)
 
+print()
+print(prefix + "Logging in...")
+
+start = perf_counter()
 login()
+end = perf_counter()
+time = end-start
+print()
+print("Done! <" + Fore.MAGENTA + Style.BRIGHT + "{:.3f}".format(time) + Fore.BLUE + Style.BRIGHT + "s>")
