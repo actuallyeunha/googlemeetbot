b1fa82e4f529277de2a118be8b92077b04a88230
Ana Eunha <66442365+actuallyeunha@users.noreply.github.com>
2020-10-09 11:33:10 -0300

    Updates and updates
    - Adicionado sistema de chamada automatica
    
    - Correcao de bugs
    
    - Comeco da implementacao de listas de links

--- start.py
+++ start.py
@@ -11,7 +11,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-import subprocess
+from yaml import load
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
 import time
 from time import sleep
 from time import perf_counter
@@ -38,25 +42,45 @@ def title():
 
 def menu():
     print()
-    print(Fore.CYAN + Style.BRIGHT + current_time)
-    print(
-        prefix + "Seja bem vinde " + Fore.MAGENTA + Style.BRIGHT + email + Fore.BLUE + Style.BRIGHT + " ao " + Fore.MAGENTA + Style.BRIGHT + "meet bot" + Fore.BLUE + Style.BRIGHT + " by: eunha" + Fore.BLUE + Style.BRIGHT + "!")
+    print(Fore.CYAN + Style.BRIGHT + "    " + current_time)
+    print(prefix + "Seja bem vinde " + special + email + default + " ao " + special + "meet bot" + default + " by: eunha!")
     print()
 
     print()
     print(prefix + "Qual navegador voce deseja usar?")
-    print(Fore.MAGENTA + Style.BRIGHT + "[f]" + Fore.BLUE + Style.BRIGHT + " Firefox")
-    print(Fore.MAGENTA + Style.BRIGHT + "[c]" + Fore.BLUE + Style.BRIGHT + " Chromium")
-    print(Fore.MAGENTA + Style.BRIGHT + "[x]" + Fore.BLUE + Style.BRIGHT + " Sair")
+    print(special + "[f]" + default + " Firefox")
+    print(special + "[c]" + default + " Chromium")
+    print(special + "[x]" + default + " Sair")
     nav = input(Fore.CYAN + Style.BRIGHT + "> ")
 
+    if nav == "x":
+        exit()
+    else:
+        pass
+
+    print()
+    print(prefix + "Quer usar a lista de: " + Fore.MAGENTA + current_day + "?")
+    print(special + "[s]" + default + " Sim")
+    print(special + Style.BRIGHT + "[n]" + default + " Nao")
+    list = input(Fore.CYAN + Style.BRIGHT + "> ")
+
     if nav == "f":
-        firefox()
+        if list == "s":
+            lista = "1"
+            firefox(lista)
+        else:
+            lista = 2
+            firefox(lista)
     elif nav == "c":
-        chrome()
+        if list == "s":
+            lista = "1"
+            chrome(lista)
+        else:
+            lista = 2
+            chrome(lista)
     elif nav == "x":
         print(
-            prefix + "Obrigada por usar o" + Fore.MAGENTA + Style.BRIGHT + "meet bot" + Fore.BLUE + Style.BRIGHT + " by: eunha")
+            prefix + "Obrigada por usar o" + special + "meet bot" + default + " by: eunha")
         print(" Ate a proxima!")
         sleep(2)
         deinit()
@@ -66,7 +90,7 @@ def menu():
         menu()
 
 
-def firefox():
+def firefox(lista):
     print()
     print(prefix + "Abrindo firefox...")
     start = perf_counter()
@@ -80,7 +104,7 @@ def firefox():
     time = end - start
 
     print()
-    print("Feito! <" + Fore.MAGENTA + Style.BRIGHT + "{:.3f}".format(time) + Fore.BLUE + Style.BRIGHT + "s>")
+    print("Feito! <" + special + "{:.3f}".format(time) + default + "s>")
 
     print()
     print(prefix + "Entrando na conta...")
@@ -93,23 +117,35 @@ def firefox():
 
     username = browser.find_element_by_id('identifierId')
     username.send_keys(email)
-    nextButton = browser.find_element_by_id('identifierNext')
-    nextButton.click()
+    nextbutton = browser.find_element_by_id('identifierNext')
+    nextbutton.click()
     sleep(2)
     password = browser.find_element_by_xpath("//input[@class='whsOnd zHQkBf']")
     password.send_keys(senha)
-    signInButton = browser.find_element_by_id('passwordNext')
-    signInButton.click()
+    signinbutton = browser.find_element_by_id('passwordNext')
+    signinbutton.click()
 
     end = perf_counter()
     time = end - start
 
     print()
-    print("Feito! <" + Fore.MAGENTA + Style.BRIGHT + "{:.3f}".format(time) + Fore.BLUE + Style.BRIGHT + "s>")
+    print("Feito! <" + special + "{:.3f}".format(time) + default + "s>")
     print()
+    if lista == "1":
+        print(prefix + "Utilizando lista de: " + Fore.MAGENTA + current_day + default + ".")
+        bparse = open("list.yaml")
+        aparse = load(bparse, Loader=Loader)
 
-    print(prefix + "Por favor, insira a Url do Meet. (ex meet.google.com/xxx-xxxx-xxx)")
-    urlmeet = input("> ")
+        for x in aparse:
+            if x == time.strftime("%H:%M", t):
+                urlmeet = x
+                break
+            else:
+                sleep(10)
+    else:
+        print(prefix + "Por favor, insira a Url do Meet. (ex meet.google.com/xxx-xxxx-xxx)")
+        urlmeet = input("> ")
+        pass
 
     print()
     print(prefix + "Entrando no meet...")
@@ -117,32 +153,33 @@ def firefox():
     browser.get("https://" + urlmeet)
     sleep(3)
     print()
-    print(prefix + "Pedindo pra entrar...")
+    print(prefix + "Entrando...")
     browser.find_element_by_xpath("//span[@class='l4V7wb Fxmcue']").click()
     end = perf_counter()
     time = end - start
 
-    print()
-    print("Feito! <" + Fore.MAGENTA + Style.BRIGHT + "{:.3f}".format(time) + Fore.BLUE + Style.BRIGHT + "s>")
+    browser.find_element_by_xpath("/html/body/div[1]/c-wiz/div[1]/div/div[5]/div[3]/div[6]/div[3]/div/div[2]/div[3]").click()
 
+    print()
+    print("Feito! <" + Fore.MAGENTA + Style.BRIGHT + "{:.3f}".format(time) + default + "s>")
     print(prefix + "Observando mensagens...")
     print()
-    chamada = 1
-    while chamada == 1:
-        try:
-            elem = browser.find_element_by_xpath("[@class='oIy2qc', @data-message-text='lalala']")
-            print("funciono caraio")
-        except NoSuchElementException:  # spelling error making this code not work as expected
-            print("pass")
-            print()
-            pass
+    while 1 == 1:
+        for element in browser.find_elements_by_class_name("oIy2qc"):
+            if element.text == "pessoa atras de mim, numero tal":
+                chatbox = element.find_element_by_xpath("/html/body/div[1]/c-wiz/div[1]/div/div[5]/div[3]/div[3]/div/div[2]/div[2]/div[2]/span[2]/div/div[3]/div[1]/div[1]/div[2]/textarea")
+                chatbox.click()
+                print(prefix + "Pessoa anterior detectada, preparando para mandar presenca em 5 segundos")
+                sleep(5)
+                chatbox.send_keys("presente" + Keys.ENTER)
+                break
+                sleep(2)
 
 
-
-def chrome():
+def chrome(lista):
     options = webdriver.ChromeOptions()
     options.add_argument("--disable-infobars")
-    options.add_argument("--window-size=1280,720")
+    options.add_argument("--window-size=800,600")
     options.add_experimental_option("prefs", {
         "profile.default_content_setting_values.media_stream_mic": 2,
         "profile.default_content_setting_values.media_stream_camera": 2,
@@ -186,8 +223,11 @@ title()
 # Important Variables
 t = time.localtime()
 current_time = time.strftime("%a | %d/%b/%y | %H:%M", t)
+current_day = time.strftime("%A", t)
 
 prefix = Fore.MAGENTA + Style.BRIGHT + "[+] " + Fore.BLUE + Style.BRIGHT
+default = Fore.BLUE + Style.BRIGHT
+special = Fore.MAGENTA + Style.BRIGHT
 
 # Get Credentials
 print(prefix + "Por favor, insira seu endereco de email. (ex: ana@eunha.com)")
